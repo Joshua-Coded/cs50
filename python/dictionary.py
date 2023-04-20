069e372d2bb5c237f97292c2a9faf15aeac58564
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-20 03:44:59 +0200

    dictionary.py

---
+++ python/dictionary.py
@@ -0,0 +1,23 @@
+words = set()
+
+def check(word):
+    if word.lower() in words:
+        return True
+    else:
+        return False
+
+def load(dictionary):
+    file = open(dictionary, "r")
+    for line in file:
+        word = line.rstrip()
+        words.add(word)
+    close(file)
+    return True
+
+def size():
+    return len(words)
+
+def unload():
+    return True
+
+    
